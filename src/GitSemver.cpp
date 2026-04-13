6b1268a7091b61042fdf8efc3b2f3bbde1891887
Mike Matthews <michael.g.matthews3@gmail.com>
2026-04-12 21:38:31 -0700

    build(scaffolding): Added basic cmake configuration

---
+++ src/GitSemver.cpp
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+/**
+ * @file GitSemver.cpp
+ * @author Mike Matthews (michael.g.matthews3@gmail.com)
+ * @brief 
+ * 
+ * @copyright Copyright (c) 2026 Mike Matthews
+ */
+
+int main() { return 0; }
